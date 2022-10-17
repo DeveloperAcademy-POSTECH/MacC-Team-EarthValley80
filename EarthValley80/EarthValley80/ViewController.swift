68052993769fccd5131b3d633146f84213725c05
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-10-17 12:08:18 +0900

    [SETTING] 초기 Project 추가

---
+++ EarthValley80/EarthValley80/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EarthValley80
+//
+//  Created by SHIN YOON AH on 2022/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
