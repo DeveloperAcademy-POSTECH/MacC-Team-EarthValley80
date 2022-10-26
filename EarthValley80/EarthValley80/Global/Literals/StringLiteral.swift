aac572b60eac178e56701da4248ef2dff8b88569
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-10-27 08:58:22 +0900

    [ADD] 육하원칙 뷰 전체 레이아웃 잡기 완료(#29)

--- EarthValley80/EarthValley80/Global/Literals/StringLiteral.swift
+++ EarthValley80/EarthValley80/Global/Literals/StringLiteral.swift
@@ -13,11 +13,17 @@ enum StringLiteral {
     
     static let readingNewsCaptionTitle = "기사읽기"
     static let inferingNewsCaptionTitle = "유추하기"
+    static let answerWhoCaptionTitle = "대답하기 | 누가"
     
     // MARK: - title
     
     static let inferingNewsTitle = "이 기사는 어떤 내용의 기사일까요?"
+    static let answerWhoTitle = "이 기사의 주인공은 누구인가요?"
+    
+    // MARK: - placeholder
+    
     static let inferingPlaceholder = "제목을 보고 떠오르는 것을 자유롭게 적어보세요"
+    static let answerWhoPlaceholder = "짧고 간단하게 대답해보세요!"
     
     // MARK: - button
     
