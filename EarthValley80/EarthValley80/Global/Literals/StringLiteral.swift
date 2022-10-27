464d9de61346bdda54c0b465e3819f6d84a6f3fd
JI SOO <dlwltn815@gmail.com>
2022-10-27 20:59:34 +0900

    [ADD] 요모조모 뉴스 타이틀 레이아웃 생성(#9)
    - customview인 MainTitleView를 사용

--- EarthValley80/EarthValley80/Global/Literals/StringLiteral.swift
+++ EarthValley80/EarthValley80/Global/Literals/StringLiteral.swift
@@ -18,6 +18,11 @@ enum StringLiteral {
     
     static let inferingNewsTitle = "이 기사는 어떤 내용의 기사일까요?"
     static let inferingPlaceholder = "제목을 보고 떠오르는 것을 자유롭게 적어보세요"
+    static let yomojomoNewsTitle = "한 주의 요모조모 뉴스"
+
+    // MARK: - title description
+
+    static let yomojomoNewsTitleDescription = "마음에 드는 뉴스를 선택하여 읽고 글을 쓸 수 있어요!"
     
     // MARK: - button
     
--- EarthValley80/EarthValley80/Screen/YomojomoNews/YomojomoNewsViewController.swift
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/YomojomoNewsViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class YomojomoNewsViewController: UIViewController {
-    
+
+    // MARK: - property
+
+    private let yomojomoTitleView: MainTitleView = {
+        let titleView = MainTitleView()
+        titleView.dateLabel.text = Date().dateFormatted("EEEE, MMM d")
+        titleView.yomojomoTitleLabel.text = StringLiteral.yomojomoNewsTitle
+        titleView.titleDescriptionLabel.text = StringLiteral.yomojomoNewsTitleDescription
+        return titleView
+    }()
+
+    // MARK: - life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupLayout()
+    }
 
+    // MARK: - func
+
+    private func setupLayout() {
+        self.view.addSubview(yomojomoTitleView)
+        yomojomoTitleView.constraint(top: self.view.safeAreaLayoutGuide.topAnchor,
+                                     leading: self.view.leadingAnchor,
+                                     trailing: self.view.leadingAnchor,
+                                     padding: UIEdgeInsets(top: 60, left: 0, bottom: 0, right: 0))
     }
 }
