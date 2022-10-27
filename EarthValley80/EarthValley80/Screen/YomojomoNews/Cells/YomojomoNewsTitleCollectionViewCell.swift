d5c6b2ff20202e225eb0be2eb8cff34acc6657ea
JI SOO <dlwltn815@gmail.com>
2022-10-27 22:57:42 +0900

    [ADD] 요모조모 뉴스 콜렉션뷰 Cell 레이아웃 수정(#9)

--- EarthValley80/EarthValley80/Screen/YomojomoNews/Cells/YomojomoNewsTitleCollectionViewCell.swift
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/Cells/YomojomoNewsTitleCollectionViewCell.swift
@@ -15,8 +15,9 @@ class YomojomoNewsTitleCollectionViewCell: UICollectionViewCell {
         let backgroundView = UIImageView()
         backgroundView.contentMode = .scaleToFill
         backgroundView.clipsToBounds = true
+        backgroundView.layer.cornerRadius = 30
         // TODO: - 임시로 색상을 넣었습니다. 추후 디자인된 png를 넣을 예정입니다.
-        backgroundView.backgroundColor = .evyGray3
+        backgroundView.backgroundColor = .evyBlack1
         return backgroundView
     }()
     let newsTitleLabel: UILabel = {
@@ -28,7 +29,11 @@ class YomojomoNewsTitleCollectionViewCell: UICollectionViewCell {
     }()
     let newsCategoryLabel: UILabel = {
         let newsCategoryLabel = UILabel()
+        newsCategoryLabel.textAlignment = .center
         newsCategoryLabel.font = .font(.bold, ofSize: 12)
+        newsCategoryLabel.layer.masksToBounds = true
+        newsCategoryLabel.layer.cornerRadius = 10
+        newsCategoryLabel.backgroundColor = .white
         // TODO: - 카테고리에 적절한 색상을 넣어야합니다.
         newsCategoryLabel.textColor = .evyBlack1
         return newsCategoryLabel
@@ -44,16 +49,18 @@ class YomojomoNewsTitleCollectionViewCell: UICollectionViewCell {
                                       trailing: self.trailingAnchor,
                                       padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
 
+        self.addSubview(newsCategoryLabel)
+        newsCategoryLabel.constraint(top: self.newsBackgroundView.topAnchor,
+                                     leading: self.newsBackgroundView.leadingAnchor,
+                                     padding: UIEdgeInsets(top: 16, left: 16, bottom: 0, right: 0))
+        self.newsCategoryLabel.constraint(.heightAnchor, constant: 20)
+        self.newsCategoryLabel.constraint(.widthAnchor, constant: 40)
+
         self.addSubview(newsTitleLabel)
-        newsTitleLabel.constraint(top: self.newsBackgroundView.topAnchor,
+        newsTitleLabel.constraint(top: self.newsCategoryLabel.bottomAnchor,
                                   leading: self.newsBackgroundView.leadingAnchor,
                                   trailing: self.newsBackgroundView.trailingAnchor,
-                                  padding: UIEdgeInsets(top: 16, left: 16, bottom: -16, right: 0))
-
-        self.addSubview(newsCategoryLabel)
-        newsCategoryLabel.constraint(leading: self.newsTitleLabel.leadingAnchor,
-                                     bottom: self.newsBackgroundView.bottomAnchor,
-                                     padding: UIEdgeInsets(top: 0, left: 0, bottom: -16, right: 0))
+                                  padding: UIEdgeInsets(top: 14, left: 20, bottom: -20, right: 0))
     }
 
     func setData(newsTitle: String, newsCategory: String) {
