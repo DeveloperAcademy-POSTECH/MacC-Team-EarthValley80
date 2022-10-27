c0e866a83bd4ade92aa9bdd7e5de4b39036b6dbe
JI SOO <dlwltn815@gmail.com>
2022-10-28 00:57:12 +0900

    [ADD] 빈공간에 넣을 Cell 생성(#39)

---
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/Cells/EmptySpaceCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  EmptySpaceCollectionViewCell.swift
+//  EarthValley80
+//
+//  Created by LeeJiSoo on 2022/10/28.
+//
+
+import UIKit
+
+class EmptySpaceCollectionViewCell: UICollectionViewCell {
+
+    // MARK: view
+
+    private let addBackgroundView: UIImageView = {
+        let backgroundView = UIImageView()
+        backgroundView.contentMode = .scaleToFill
+        backgroundView.clipsToBounds = true
+        backgroundView.layer.cornerRadius = 30
+        // TODO: - 카테고리에 적절한 색상을 넣어야합니다.
+        backgroundView.backgroundColor = .red
+        return backgroundView
+    }()
+
+    // MARK: method
+
+    func setupLayout() {
+        self.addSubview(addBackgroundView)
+        self.addBackgroundView.constraint(to: self,
+                                          insets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+    }
+}
