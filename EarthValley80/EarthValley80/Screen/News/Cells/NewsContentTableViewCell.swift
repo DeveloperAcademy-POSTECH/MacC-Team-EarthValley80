ad38a930d7d58ae36c0eff4d7c401401bf4cd039
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-10-27 14:54:51 +0900

    [FEAT] 글을 문장으로 자르는 로직 추가(#31)

--- EarthValley80/EarthValley80/Screen/News/Cells/NewsContentTableViewCell.swift
+++ EarthValley80/EarthValley80/Screen/News/Cells/NewsContentTableViewCell.swift
@@ -79,7 +79,7 @@ final class NewsContentTableViewCell: UITableViewCell {
         let label = UILabel()
         label.numberOfLines = 0
         
-        //TODO: - 더미 데이터, 나중에 지우겠습니다.
+        // TODO: - 더미 데이터
         label.text = """
         ‘타다’는 승합차를 유료로 타려는 이용자와 운전자를 연결해주는 차량공유 앱 서비스입니다. 승합차는 일반 택시보다 크고 마을버스보다 작은 차종을 말합니다. 대개 11~15인승입니다. 2018년 10월 ‘타다’라는 글자를 새긴 차가 처음 시장에 등장했습니다.
         미국에서 차량공유 서비스인 ‘우버’가 주목받은 터여서 타다는 한국식 우버로 불리기도 했습니다.
@@ -102,12 +102,14 @@ final class NewsContentTableViewCell: UITableViewCell {
             self.setupLayout(status: newValue)
         }
     }
+    private var sentences: [String] = []
 
     // MARK: - init
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.configureUI()
+        self.appendSentences()
     }
     
     @available(*, unavailable)
@@ -126,5 +128,15 @@ final class NewsContentTableViewCell: UITableViewCell {
     private func configureUI() {
         self.backgroundColor = .clear
         self.selectionStyle = .none
+    }
+    
+    private func appendSentences() {
+        guard let content = self.contentLabel.text else { return }
+        self.sentences = self.makeSentences(with: content)
+    }
+    
+    private func makeSentences(with content: String) -> [String] {
+        guard content == "" else { return [] }
+        return content.components(separatedBy: [".", "?", "!"]).map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }.filter { !$0.isEmpty }
     }
 }
