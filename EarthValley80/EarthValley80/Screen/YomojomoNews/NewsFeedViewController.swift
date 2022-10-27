bb30483234d6f9387e7f8d9049f2451e19594d23
JI SOO <dlwltn815@gmail.com>
2022-10-27 19:52:50 +0900

    [CHORE] YomojomoNews로 이름 변경(#9)

---
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/NewsFeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  YomojomoNewsViewController.swift
+//  EarthValley80
+//
+//  Created by SHIN YOON AH on 2022/10/17.
+//
+
+import UIKit
+
+class YomojomoNewsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
