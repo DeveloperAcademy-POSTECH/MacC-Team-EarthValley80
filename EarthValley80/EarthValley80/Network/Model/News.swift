fdb3e89ecfb7fa148be7c1429a981ec00e4ae365
JI SOO <dlwltn815@gmail.com>
2022-10-30 00:59:18 +0900

    [CHORE] Model News 변수명 변경(#9)

--- EarthValley80/EarthValley80/Network/Model/News.swift
+++ EarthValley80/EarthValley80/Network/Model/News.swift
@@ -8,31 +8,31 @@
 import UIKit
 
 struct News {
-    let newsTitle: String
-    let newsCategory: String
+    let title: String
+    let category: String
 }
 
 //TODO: - 더미 데이터, 나중에 지우겠습니다.
 var yomojomoViewDummyData: [News] = [
-    News(newsTitle: "1인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "3인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "3인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "3인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "3인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", newsCategory: "시사"),
-    News(newsTitle: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "2급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사"),
-    News(newsTitle: "3급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", newsCategory: "시사")
+    News(title: "1인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "2인류보다 로봇 진화 속도가 더 빠르대요, 청소로봇은 '루시'…생각하는 로봇 등장", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
+    News(title: "1급락한 카카오뱅크 주식, 카뱅이 사들이는 이유는", category: "시사"),
     ]
