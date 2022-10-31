7c34f4dd2f1d0165b34f461c29123761dda69884
JI SOO <dlwltn815@gmail.com>
2022-10-31 09:51:00 +0900

    [CHORE] News Model 내부로 정렬 로직 이동(#39)

--- EarthValley80/EarthValley80/Screen/YomojomoNews/YomojomoNewsViewController.swift
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/YomojomoNewsViewController.swift
@@ -36,7 +36,9 @@ final class YomojomoNewsViewController: UIViewController {
         collectionView.register(EmptySpaceCollectionViewCell.self, forCellWithReuseIdentifier: EmptySpaceCollectionViewCell.className)
         return collectionView
     }()
-    private lazy var newsData: [News] = self.arrangeNewsData(yomojomoViewDummyData)
+
+    private let newsModel = News(title: nil, category: nil)
+    private lazy var newsData = newsModel.arrangeNewsData(yomojomoViewDummyData)
 
     // MARK: - life cycle
 
@@ -60,112 +62,6 @@ final class YomojomoNewsViewController: UIViewController {
                                   bottom: self.view.bottomAnchor,
                                   trailing: self.view.trailingAnchor,
                                   padding: UIEdgeInsets(top: 37, left: 0, bottom: 0, right: 0))
-    }
-
-    private func arrangeNewsData(_ data: [News]) -> [News] {
-        let shortNews = data.filter { $0.title?.count ?? 0 < Size.standardOfTitle }.map { $0 }
-        let longNews = data.filter { $0.title?.count ?? 0 >= Size.standardOfTitle }.map { $0 }
-        var newdata: [News] = []
-        if longNews.count / 2 <= shortNews.count {
-            newdata = self.calculateMoreShortNews(shortNews, longNews)
-        } else {
-            newdata = self.calculateMoreLongNews(shortNews, longNews)
-        }
-        return newdata
-    }
-
-    private func calculateMoreLongNews( _ shortNews: [News], _ longNews: [News]) -> [News] {
-        var newdata: [News] = []
-        var longNews = longNews
-        var shortNews = shortNews
-        let longcount = longNews.count
-        var needShortCount = 0
-
-        if longNews.count % 2 == 0 {
-            needShortCount = longcount/2  - (shortNews.count+1)
-        } else {
-            needShortCount = longcount/2  - (shortNews.count)
-        }
-
-        for _ in 0..<needShortCount {
-            shortNews.insert(News(title: nil, category: nil), at: 0)
-//            shortNews.insert(News(title: "-", category: "-"), at: 0)
-        }
-
-        for rowNum in 0..<longcount/2 {
-            if rowNum % 3 == 0 {
-                newdata.append(longNews.removeLast())
-                newdata.append(longNews.removeLast())
-                if rowNum == (longcount/2-1) {
-                    continue
-                } else {
-                    newdata.append(shortNews.removeLast())
-                }
-            } else if rowNum % 3 == 1 {
-                if rowNum == (longcount/2-1) {
-                    continue
-                } else {
-                    newdata.append(shortNews.removeLast())
-                }
-                newdata.append(longNews.removeLast())
-                newdata.append(longNews.removeLast())
-            } else {
-                newdata.append(longNews.removeLast())
-                if rowNum == (longcount/2-1) {
-                    continue
-                } else {
-                    newdata.append(shortNews.removeLast())
-                }
-                newdata.append(longNews.removeLast())
-            }
-        }
-
-        if !longNews.isEmpty {
-            newdata.append(longNews.removeLast())
-        }
-
-        if !shortNews.isEmpty {
-            for _ in 0..<shortNews.count {
-                newdata.append(shortNews.removeLast())
-            }
-        }
-
-        return newdata
-    }
-
-    private func calculateMoreShortNews(_ shortNews: [News], _ longNews: [News]) -> [News] {
-        var newdata: [News] = []
-        var longNews = longNews
-        var shortNews = shortNews
-        let longcount = longNews.count
-
-        for rowNum in 0..<longcount/2 {
-            if rowNum % 3 == 0 {
-                newdata.append(longNews.removeLast())
-                newdata.append(longNews.removeLast())
-                newdata.append(shortNews.removeLast())
-            } else if rowNum % 3 == 1 {
-                newdata.append(shortNews.removeLast())
-                newdata.append(longNews.removeLast())
-                newdata.append(longNews.removeLast())
-            } else {
-                newdata.append(longNews.removeLast())
-                newdata.append(shortNews.removeLast())
-                newdata.append(longNews.removeLast())
-            }
-        }
-
-        if !longNews.isEmpty {
-            newdata.append(longNews.removeLast())
-        }
-
-        if !shortNews.isEmpty {
-            for _ in 0..<shortNews.count {
-                newdata.append(shortNews.removeLast())
-            }
-        }
-
-        return newdata
     }
 }
 
