11c54f52873f978f81f31884964b18c8a7e64f46
JI SOO <dlwltn815@gmail.com>
2022-11-03 23:57:10 +0900

    [CHORE] 강제 타입캐스팅 없애기(#53)

--- EarthValley80/EarthValley80/Screen/MyNews/Helper/LeftAlignCollectionViewFlowLayout.swift
+++ EarthValley80/EarthValley80/Screen/MyNews/Helper/LeftAlignCollectionViewFlowLayout.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 final class LeftAlignCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    let cellSpacing: CGFloat = 12.0
 
-    override func layoutAttributesForElements(in rect: CGRect) ->  [UICollectionViewLayoutAttributes]? {
-        let attributes = super.layoutAttributesForElements(in: rect)?.map { $0.copy() as! UICollectionViewLayoutAttributes }
-        var leftMargin: CGFloat = 0.0
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        self.sectionInset = UIEdgeInsets.zero
+        let attributes = super.layoutAttributesForElements(in: rect)
+
+        var leftMargin = sectionInset.left
         var maxY: CGFloat = -1.0
-
         attributes?.forEach { layoutAttribute in
-            guard layoutAttribute.representedElementCategory == .cell else {
-                return
-            }
-
             if layoutAttribute.frame.origin.y >= maxY {
-                leftMargin = 0.0
+                leftMargin = sectionInset.left
             }
             layoutAttribute.frame.origin.x = leftMargin
-            leftMargin += layoutAttribute.frame.width + self.minimumInteritemSpacing
+            leftMargin += layoutAttribute.frame.width + cellSpacing
             maxY = max(layoutAttribute.frame.maxY, maxY)
         }
         return attributes
