b1613a5adc34e829619d5f4ef55d216f23f870bb
JI SOO <dlwltn815@gmail.com>
2022-11-03 19:16:41 +0900

    [CHORE] button 내부 글자 크기 및 font 설정(#45)

--- EarthValley80/EarthValley80/Global/UIComponent/GotoSomewhereButton.swift
+++ EarthValley80/EarthValley80/Global/UIComponent/GotoSomewhereButton.swift
@@ -13,6 +13,7 @@ final class GotoSomewhereButton: UIButton {
         static let buttonContentEdgeIneset: CGFloat = -10.0
         static let buttonFontSize: CGFloat = 16.0
         static let buttonHeigth: CGFloat = 50.0
+        static let buttonCornerRadius: CGFloat = 25.0
         static let buttonImageSize: UIImage.SymbolConfiguration = UIImage.SymbolConfiguration(pointSize: 35)
     }
 
@@ -33,19 +34,24 @@ final class GotoSomewhereButton: UIButton {
     private func configureUI() {
         if #available(iOS 15.0, *) {
             var buttonConfig = UIButton.Configuration.filled()
+            var buttonTitleAttribute = AttributedString()
+            buttonTitleAttribute.font = .font(.medium, ofSize: 16)
+            buttonConfig.attributedTitle = buttonTitleAttribute
             buttonConfig.titleAlignment = .leading
             buttonConfig.baseBackgroundColor = .evyBlack1
             buttonConfig.cornerStyle = .capsule
             buttonConfig.preferredSymbolConfigurationForImage = Size.buttonImageSize
-            buttonConfig.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 10, bottom: 8, trailing: 30)
+            buttonConfig.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 10, bottom: 7, trailing: 30)
             configuration = buttonConfig
         } else {
             self.invalidateIntrinsicContentSize()
             self.titleLabel?.textAlignment = .center
+            self.titleLabel?.setLineSpacing(kernValue: -0.32, lineHeightMultiple: 0.83)
             self.backgroundColor = .evyBlack1
             self.imageView?.contentMode = .scaleAspectFit
             self.constraint(.heightAnchor, constant: Size.buttonHeigth)
-            self.contentEdgeInsets = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 30)
+            self.contentEdgeInsets = UIEdgeInsets(top: 8, left: 10, bottom: 7, right: 30)
+            self.layer.cornerRadius = Size.buttonCornerRadius
         }
     }
 }
