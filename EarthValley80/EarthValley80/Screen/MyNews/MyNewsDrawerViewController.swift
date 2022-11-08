19fb3891ab7f1120db34017bfbe58e8380566719
JI SOO <dlwltn815@gmail.com>
2022-11-09 02:29:19 +0900

    [FEAT] attainmentLabel text에 들어갈 수 있도록 index계산하는 로직 추가(#64)

--- EarthValley80/EarthValley80/Screen/MyNews/MyNewsDrawerViewController.swift
+++ EarthValley80/EarthValley80/Screen/MyNews/MyNewsDrawerViewController.swift
@@ -43,6 +43,7 @@ final class MyNewsDrawerViewController: UIViewController {
     // TODO: - 더미데이터 입니다. 추후 변경 예정입니다. nil데이터를 추가해주고 다시 db에 저장해주어야합니다.
     private let newsModel = NewsSortingManager()
     private lazy var myNewsData: [News] = self.newsModel.appendNilDataForMyNewsDrawer(yomojomoViewDummyData)
+    private lazy var totalCountExcludeNil: Int = self.myNewsData.filter{ ($0.title != nil) }.count
 
     // MARK: - init
 
@@ -67,6 +68,11 @@ final class MyNewsDrawerViewController: UIViewController {
                                   trailing: self.view.trailingAnchor,
                                   padding: UIEdgeInsets(top: 37, left: 0, bottom: 0, right: 0))
     }
+
+    private func calculateLabelIndex(indexPath: Int) -> Int {
+        let fromZeroToindex = self.myNewsData[0...indexPath]
+        return self.totalCountExcludeNil - fromZeroToindex.filter{ $0.title != nil }.count
+    }
 }
 
 // MARK: - UICollectionViewDataSource
@@ -77,10 +83,12 @@ extension MyNewsDrawerViewController: UICollectionViewDataSource {
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if myNewsData[indexPath.row].title == nil {
+            let labelIndex: Int = self.calculateLabelIndex(indexPath: indexPath.row)
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UserGoalAttainmentLottieCollectionViewCell.className, for: indexPath) as? UserGoalAttainmentLottieCollectionViewCell else { return UICollectionViewCell() }
             // TODO: - 테플에서는 로티 4개정도 random, 나중에 수정하겠습니다.
             let lottieNumber = Int.random(in: 0...3)
             cell.setLottieImage(to: "myNewsDrawerLottie\(lottieNumber)")
+            cell.setLottieLabel(index: labelIndex)
             return cell
         } else {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyNewsDrawerCollectionViewCell.className, for: indexPath) as? MyNewsDrawerCollectionViewCell else { return UICollectionViewCell() }
