0e0ff2d5641963163fb3e33175a05f328951c9ec
JI SOO <dlwltn815@gmail.com>
2022-11-11 12:07:53 +0900

    [FEAT] alert 레이아웃 설정(#54)

--- EarthValley80/EarthValley80/Global/UIComponent/AlertViewController.swift
+++ EarthValley80/EarthValley80/Global/UIComponent/AlertViewController.swift
@@ -83,18 +83,55 @@ class AlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureUI()
+        setupLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        // curveEaseOut: 시작은 천천히, 끝날 땐 빠르게
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveEaseOut) { [weak self] in
+            self?.containerView.transform = .identity
+            self?.containerView.isHidden = false
+        }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        // curveEaseIn: 시작은 빠르게, 끝날 땐 천천히
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveEaseIn) { [weak self] in
+            self?.containerView.transform = .identity
+            self?.containerView.isHidden = true
+        }
     }
 
     // MARK: - func
+
+    private func configureUI() {
+        view.backgroundColor = .black.withAlphaComponent(0.2)
+    }
+
+    private func setupLayout() {
+        self.view.addSubview(containerView)
+        self.containerView.constraint(centerX: self.view.centerXAnchor,
+                                      centerY: self.view.centerYAnchor,
+                                      padding: UIEdgeInsets.zero)
+        self.containerView.constraint(.widthAnchor, constant: Size.alertWidth)
+        self.containerView.constraint(.heightAnchor, constant: Size.alertHeight)
+
+        self.containerView.addSubview(containerStackView)
+        self.containerStackView.constraint(.widthAnchor, constant: Size.alertWidth)
+        self.containerStackView.constraint(.heightAnchor, constant: Size.alertHeight)
+        self.containerStackView.addArrangedSubview(titleStackView)
+        self.containerStackView.addArrangedSubview(alertButtonStackView)
+
+        self.titleStackView.addArrangedSubview(alertImageView)
+        self.titleStackView.addArrangedSubview(alertSmallTitle)
+        self.titleStackView.addArrangedSubview(alertBigTitle)
+        self.titleStackView.setCustomSpacing(10, after: alertImageView)
+        self.titleStackView.setCustomSpacing(6, after: alertSmallTitle)
+        self.alertImageView.constraint(.widthAnchor, constant: 82)
+        self.alertImageView.constraint(.heightAnchor, constant: 60)
+    }
 
 }
