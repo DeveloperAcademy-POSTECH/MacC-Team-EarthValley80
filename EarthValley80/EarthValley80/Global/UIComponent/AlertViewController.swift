2d9d87b99653e9a85fcce9c1552eb14771774932
JI SOO <dlwltn815@gmail.com>
2022-11-11 12:09:06 +0900

    [CHORE] lazy var 로 수정(#54)

--- EarthValley80/EarthValley80/Global/UIComponent/AlertViewController.swift
+++ EarthValley80/EarthValley80/Global/UIComponent/AlertViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class AlertViewController: UIViewController {
 
     private enum Size {
-
+        static let alertWidth: CGFloat = 270
+        static let alertHeight: CGFloat = 200 // 버튼갯수 * 높이로 조절
     }
 
     // MARK: - property
@@ -20,7 +21,7 @@ class AlertViewController: UIViewController {
     private var bigTitle: String?
     private var contentView: UIView?
 
-    private let containerView: UIView = {
+    private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .evyWhite
         view.layer.cornerRadius = 16
@@ -28,50 +29,53 @@ class AlertViewController: UIViewController {
         view.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
         return view
     }()
-    private let containerStackView: UIStackView = {
+    private lazy var containerStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 24
         return stackView
     }()
-    private let titleStackView: UIStackView = {
+    private lazy var titleStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
+        stackView.alignment = .center
         return stackView
     }()
-    private let alertButtonStackView: UIStackView = {
+    private lazy var alertButtonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
+        stackView.spacing = 0
+        stackView.distribution = .fillEqually
         return stackView
     }()
-    private let actionSheetButtonStackView: UIStackView = {
+    private lazy var actionSheetButtonStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         return stackView
     }()
-    private let buttonDivider: UIView = {
+    private lazy var buttonDivider: UIView = {
         let view = UIView()
         view.backgroundColor = .evyGray3
         return view
     }()
     // TODO: - 추후 로티view로 변경되어야 합니다.
-    private let alertImageView: UIImageView = {
+    private lazy var alertImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.backgroundColor = .red
         return imageView
     }()
-    private let alertSmallTitle: UILabel = {
+    private lazy var alertSmallTitle: UILabel = {
         let label = UILabel()
         return label
     }()
-    private let alertBigTitle: UILabel = {
+    private lazy var alertBigTitle: UILabel = {
         let label = UILabel()
         return label
     }()
 
     // MARK: - init
 
-    convenience init(lottieName: String, smallTitle: String, bitTitle: String) {
+    convenience init(lottieName: String, smallTitle: String, bigTitle: String) {
         self.init()
         self.lottieName = lottieName
         self.smallTitle = smallTitle
