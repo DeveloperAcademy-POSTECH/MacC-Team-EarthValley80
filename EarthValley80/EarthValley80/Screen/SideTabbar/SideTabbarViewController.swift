fa177fc706c8e10df7c52efc4a6a4ffc1136f947
SeonJeon <letschutzpah430@gmail.com>
2022-11-14 16:24:29 +0900

    [CORRECT] self 붙이기

--- EarthValley80/EarthValley80/Screen/SideTabbar/SideTabbarViewController.swift
+++ EarthValley80/EarthValley80/Screen/SideTabbar/SideTabbarViewController.swift
@@ -15,20 +15,19 @@ final class SideTabbarViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.setupLayout()
     }
     
     private func setupLayout() {
-        addChild(sideTabbar)
+        addChild(self.sideTabbar)
         self.view.addSubview(self.sideTabbar.view)
-        sideTabbar.didMove(toParent: self)
+        self.sideTabbar.didMove(toParent: self)
         self.sideTabbar.view.constraint(top: self.view.topAnchor, leading: self.view.leadingAnchor, bottom: self.view.bottomAnchor, trailing: self.view.trailingAnchor)
         
-        addChild(embeddedViewController)
-        self.view.addSubview(embeddedViewController.view)
-        embeddedViewController.didMove(toParent: self)
-        embeddedViewController.view.constraint(top: self.view.topAnchor, leading: self.view.leadingAnchor, bottom: self.view.bottomAnchor, trailing: self.view.trailingAnchor, padding: UIEdgeInsets(top: 20, left: 254, bottom: 20, right: 50))
+        addChild(self.embeddedViewController)
+        self.view.addSubview(self.embeddedViewController.view)
+        self.embeddedViewController.didMove(toParent: self)
+        self.embeddedViewController.view.constraint(top: self.view.topAnchor, leading: self.view.leadingAnchor, bottom: self.view.bottomAnchor, trailing: self.view.trailingAnchor, padding: UIEdgeInsets(top: 20, left: 254, bottom: 20, right: 50))
     }
 }
 
