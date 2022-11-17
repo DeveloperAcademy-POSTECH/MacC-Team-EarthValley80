d2a2f0df4d0ab9e4382cd7488f238e7f462cfc47
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-17 19:33:56 +0900

    [ADD] Reaction Button 생성(#119)

---
+++ EarthValley80/EarthValley80/Screen/News/Components/ReactionButton.swift
@@ -0,0 +1,27 @@
+//
+//  ReactionButton.swift
+//  EarthValley80
+//
+//  Created by SHIN YOON AH on 2022/11/17.
+//
+
+import UIKit
+
+final class ReactionButton: UIButton {
+
+    // MARK: - property
+
+    
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
