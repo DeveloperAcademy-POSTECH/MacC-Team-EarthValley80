33b17be966231003fff4b99ba4391bb090074c54
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-17 10:06:32 +0900

    [ADD] reaction view controller 생성(#119)

---
+++ EarthValley80/EarthValley80/Screen/News/ReactionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ReactionViewController.swift
+//  EarthValley80
+//
+//  Created by SHIN YOON AH on 2022/11/17.
+//
+
+import UIKit
+
+final class ReactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
