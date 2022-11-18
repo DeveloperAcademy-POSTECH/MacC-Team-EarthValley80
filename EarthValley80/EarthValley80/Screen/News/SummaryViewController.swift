66e9d4c435a19982df2464fe08b9cd78880be4ac
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-18 10:35:59 +0900

    [ADD] Summary View Controller 생성(#128)

---
+++ EarthValley80/EarthValley80/Screen/News/SummaryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SummaryViewController.swift
+//  EarthValley80
+//
+//  Created by SHIN YOON AH on 2022/11/18.
+//
+
+import UIKit
+
+final class SummaryViewController: UIViewController {
+
+    // MARK: - property
+
+
+
+    // MARK: - life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupLayout()
+    }
+
+    // MARK: - func
+
+    private func setupLayout() {
+
+    }
+}
