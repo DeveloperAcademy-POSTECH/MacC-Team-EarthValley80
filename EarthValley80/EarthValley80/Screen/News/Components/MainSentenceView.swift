da128a90457473758ff03d38ee599fb1f01ef711
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-20 16:21:07 +0900

    [ADD] TableView 생성(#132)

--- EarthValley80/EarthValley80/Screen/News/Components/MainSentenceView.swift
+++ EarthValley80/EarthValley80/Screen/News/Components/MainSentenceView.swift
@@ -14,6 +14,26 @@ final class MainSentenceView: UIView {
         static let width: CGFloat = 573.adjustedWidth
     }
 
+    // MARK: - property
+
+    private lazy var sentenceTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.dataSource = self
+        tableView.backgroundColor = .red
+        tableView.separatorColor = .clear
+        tableView.indicatorStyle = .white
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.contentInset = UIEdgeInsets(top: 40, left: 0, bottom: 50, right: 0)
+        tableView.register(cell: NewsContentTableViewCell.self)
+
+        if #available(iOS 15.0, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
+
+        return tableView
+    }()
+
     // MARK: - init
 
     override init(frame: CGRect) {
@@ -32,11 +52,25 @@ final class MainSentenceView: UIView {
     private func setupLayout() {
         self.constraint(.heightAnchor, constant: Size.height)
         self.constraint(.widthAnchor, constant: Size.width)
+
+        self.addSubview(self.sentenceTableView)
+        self.sentenceTableView.constraint(to: self)
     }
 
     private func configureUI() {
         self.backgroundColor = .white
         self.layer.cornerRadius = 30
         self.layer.masksToBounds = true
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension MainSentenceView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
