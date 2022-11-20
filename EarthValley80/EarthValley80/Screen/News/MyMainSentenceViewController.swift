f09ec18499d3e868c41a13c4b8674e227ecc60ec
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-20 22:45:23 +0900

    [FEAT] close button, main sentence button에 action 추가(#136)

--- EarthValley80/EarthValley80/Screen/News/MyMainSentenceViewController.swift
+++ EarthValley80/EarthValley80/Screen/News/MyMainSentenceViewController.swift
@@ -19,7 +19,7 @@ final class MyMainSentenceViewController: UIViewController {
         let button = UIButton()
 
         // TODO: - 나중에 Asset 이미지 변경
-        let imageView = UIImageView(image: UIImage(systemName: "pencil.circle"))
+        let imageView = UIImageView(image: UIImage(systemName: "pencil.circle.fill"))
         button.addSubview(imageView)
         imageView.constraint(top: button.topAnchor, centerX: button.centerXAnchor)
         imageView.constraint(.heightAnchor, constant: Size.buttonSize)
@@ -35,7 +35,7 @@ final class MyMainSentenceViewController: UIViewController {
         super.viewDidLoad()
         self.setupLayout()
         self.configureUI()
-
+        self.setupButtonAction()
     }
 
     // MARK: - func
@@ -44,7 +44,9 @@ final class MyMainSentenceViewController: UIViewController {
         self.view.addSubview(self.closeButton)
         self.closeButton.constraint(top: self.view.topAnchor,
                                              trailing: self.view.trailingAnchor,
-                                             padding: UIEdgeInsets(top: 46, left: 0, bottom: 0, right: 60))
+                                             padding: UIEdgeInsets(top: 46, left: 0, bottom: 0, right: 65))
+        self.closeButton.constraint(.heightAnchor, constant: Size.buttonSize)
+        self.closeButton.constraint(.widthAnchor, constant: Size.buttonSize)
 
         self.view.addSubview(self.mainSentenceView)
         self.mainSentenceView.constraint(centerX: self.view.centerXAnchor,
@@ -54,5 +56,13 @@ final class MyMainSentenceViewController: UIViewController {
     private func configureUI() {
         // TODO: - 컬러셋 정해지면 변경
         self.view.backgroundColor = .black.withAlphaComponent(0.94)
+    }
+
+    private func setupButtonAction() {
+        let dismissAction = UIAction { [weak self] _ in
+            self?.dismiss(animated: true)
+        }
+
+        self.closeButton.addAction(dismissAction, for: .touchUpInside)
     }
 }
