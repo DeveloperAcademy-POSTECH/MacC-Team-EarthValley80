06053a042ebe30b0a9005ac97f6024146244b36d
JI SOO <dlwltn815@gmail.com>
2022-11-20 23:39:20 +0900

    [ADD] 요모조모 뉴스 콜렉션뷰 셀 레이아웃 설정(#113)

--- EarthValley80/EarthValley80/Screen/YomojomoNews/Cells/YomojomoNewsCollectionViewCell.swift
+++ EarthValley80/EarthValley80/Screen/YomojomoNews/Cells/YomojomoNewsCollectionViewCell.swift
@@ -52,4 +52,51 @@ class YomojomoNewsCollectionViewCell: UICollectionViewCell {
         label.text = "이건뉴스 기사 제목이지롱 어때 이건 바로 테스트야!"
         return label
     }()
+
+    // MARK: - init
+
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+        self.setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - func
+
+    private func setupLayout() {
+        self.contentView.addSubview(self.cellBackground)
+        self.cellBackground.constraint(to: self)
+
+        self.cellBackground.addSubview(self.categoryBackgroundImageView)
+        self.categoryBackgroundImageView.constraint(top: self.cellBackground.topAnchor,
+                                                    trailing: self.cellBackground.trailingAnchor,
+                                                    padding: UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0))
+        self.categoryBackgroundImageView.constraint(.widthAnchor, constant: 200)
+        self.categoryBackgroundImageView.constraint(.heightAnchor, constant: 200)
+
+        self.cellBackground.addSubview(self.newsImageView)
+        self.newsImageView.constraint(top: self.cellBackground.topAnchor,
+                                      leading: self.cellBackground.leadingAnchor,
+                                      padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+        self.newsImageView.constraint(.widthAnchor, constant: 233)
+        self.newsImageView.constraint(.heightAnchor, constant: 233)
+
+        self.cellBackground.addSubview(self.newsCategoryLabel)
+        self.newsCategoryLabel.constraint(top: self.newsImageView.bottomAnchor,
+                                          leading: self.newsImageView.leadingAnchor,
+                                          padding: UIEdgeInsets(top: 30, left: 0, bottom: 0, right: 0))
+        self.newsCategoryLabel.constraint(.heightAnchor, constant: 26)
+
+        self.cellBackground.addSubview(self.newsTitleLabel)
+        self.newsTitleLabel.constraint(top: self.newsCategoryLabel.bottomAnchor,
+                                       leading: self.newsCategoryLabel.leadingAnchor,
+                                       bottom: self.cellBackground.bottomAnchor,
+                                       trailing: self.cellBackground.trailingAnchor,
+                                       padding: UIEdgeInsets(top: 8, left: 0, bottom: 0, right: 0))
+    }
+
 }
