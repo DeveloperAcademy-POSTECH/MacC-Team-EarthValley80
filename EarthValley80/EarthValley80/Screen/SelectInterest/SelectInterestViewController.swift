0377978a1f34d148292641b72edbc16b007f64c1
JI SOO <dlwltn815@gmail.com>
2022-11-23 04:13:40 +0900

    [ADD] select 관심사 뷰에 들어갈 뷰 프로퍼티 생성(#149)

--- EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
+++ EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
@@ -9,4 +9,34 @@ import UIKit
 
 class SelectInterestViewController: UIViewController {
 
+    // MARK: - property
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollview = UIScrollView()
+        scrollview.backgroundColor = .evyWhite
+        scrollview.showsVerticalScrollIndicator = true
+        scrollview.showsHorizontalScrollIndicator = true
+        scrollview.contentSize = CGSize(width: UIScreen.main.bounds.width * 1.3, height: UIScreen.main.bounds.height * 1.3)
+        return scrollview
+    }()
+    private let contentView = UIImageView()
+    private let greetingLabel: UILabel = {
+        let label = UILabel()
+        label.text = StringLiteral.greetingTitle
+        label.font = .font(.regular, ofSize: 12)
+        return label
+    }()
+    private let selectGuideTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = StringLiteral.selectGuideTitle
+        label.font = .font(.title, ofSize: 40)
+        label.numberOfLines = 0
+        return label
+    }()
+
+    // MARK: - life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
