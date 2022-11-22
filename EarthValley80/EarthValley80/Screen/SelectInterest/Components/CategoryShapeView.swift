e73c546b458e92edf55d0d3f0475b2fdccdadeb4
JI SOO <dlwltn815@gmail.com>
2022-11-23 04:16:11 +0900

    [ADD] category shape 컴포넌트 생성(#149)

---
+++ EarthValley80/EarthValley80/Screen/SelectInterest/Components/CategoryShapeView.swift
@@ -0,0 +1,58 @@
+//
+//  CategoryShapeView.swift
+//  EarthValley80
+//
+//  Created by LeeJiSoo on 2022/11/23.
+//
+
+import UIKit
+
+class CategoryShapeView: UIView {
+
+    // MARK: - property
+
+    private let backgroundImage: UIImageView = {
+        let backgroundview = UIImageView()
+        backgroundview.contentMode = .scaleAspectFit
+        return backgroundview
+    }()
+    private let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .evyWhite
+        // TODO: - 화면 비율에 맞게 글자 크기도 조절해줘야합니다.
+        label.font = .font(.regular, ofSize: 18)
+        label.textAlignment = .center
+        return label
+    }()
+
+    // MARK: - init
+
+    init(image: UIImage, labelText: String) {
+        super.init(frame: .zero)
+        self.backgroundImage.image = image
+        self.categoryLabel.text = labelText
+        self.setupLayout()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    // MARK: - func
+
+    private func setupLayout() {
+        self.addSubview(self.backgroundImage)
+        self.backgroundImage.constraint(top: self.topAnchor,
+                                        leading: self.leadingAnchor,
+                                        padding: UIEdgeInsets.zero)
+        self.backgroundImage.constraint(.widthAnchor, constant: 200.adjustedWidth)
+        self.backgroundImage.constraint(.heightAnchor, constant: 200.adjustedHeight)
+
+
+        self.addSubview(self.categoryLabel)
+        self.categoryLabel.constraint(centerX: self.backgroundImage.centerXAnchor,
+                                      centerY: self.backgroundImage.centerYAnchor,
+                                      padding: UIEdgeInsets.zero)
+    }
+}
