12c8e752d01fd73e356153dd59739c845b8cdfd1
JI SOO <dlwltn815@gmail.com>
2022-11-23 05:00:15 +0900

    [CHORE] self 키워드 추가(#149)

--- EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
+++ EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
@@ -55,7 +55,7 @@ class SelectInterestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLayout()
+        self.setupLayout()
     }
 
     // MARK: - func
