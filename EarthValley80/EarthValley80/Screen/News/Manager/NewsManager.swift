75a234a7376226d677e98716c1251ae3db388ead
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-11-27 14:55:12 +0900

    [CHORE] 기사 타이틀과 이미지를 유추 뷰에 반영(#166)

--- EarthValley80/EarthValley80/Screen/News/Manager/NewsManager.swift
+++ EarthValley80/EarthValley80/Screen/News/Manager/NewsManager.swift
@@ -5,7 +5,7 @@
 //  Created by SHIN YOON AH on 2022/11/23.
 //
 
-import Foundation
+import UIKit
 
 final class NewsManager {
 
@@ -15,6 +15,7 @@ final class NewsManager {
 
     private(set) var newsTitle: String = ""
     private(set) var newsContent: String = ""
+    private(set) var newsImage: UIImage = UIImage()
     private(set) var mainSentences: [String] = []
 
     // MARK: - init
@@ -26,12 +27,14 @@ final class NewsManager {
     func resetAll() {
         self.newsTitle = ""
         self.newsContent = ""
+        self.newsImage = UIImage()
         self.mainSentences.removeAll()
     }
 
-    func setupNews(title: String, content: String) {
+    func setupNews(title: String, content: String, image: UIImage) {
         self.newsTitle = title
         self.newsContent = content
+        self.newsImage = image
     }
 
     func setupMainSentences(_ sentences: [String]) {
