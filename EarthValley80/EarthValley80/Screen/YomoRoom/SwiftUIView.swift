5907b4002d80af907d745e1e14032d8e6a54453f
SeonJeon <letschutzpah430@gmail.com>
2022-11-30 23:05:13 +0900

    [CHORE] 기존 요모의 방 가기 텍스트를 버튼으로 교체

---
+++ EarthValley80/EarthValley80/Screen/YomoRoom/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  EarthValley80
+//
+//  Created by seojeon22 on 2022/11/30.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
