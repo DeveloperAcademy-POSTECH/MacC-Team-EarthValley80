bfdc64a3581cb1fe93f0c10dd9c3233e95977707
JI SOO <dlwltn815@gmail.com>
2022-12-12 01:05:15 +0900

    [ADD] spriteKit에서 사용할 bubble ui 추가(#196)

--- EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
+++ EarthValley80/EarthValley80/Screen/SelectInterest/SelectInterestViewController.swift
@@ -5,11 +5,13 @@
 //  Created by LeeJiSoo on 2022/11/23.
 //
 
+import SpriteKit
 import UIKit
 
 final class SelectInterestViewController: UIViewController {
 
     // MARK: - property
+    
     private let greetingLabel: UILabel = {
         let label = UILabel()
         label.text = StringLiteral.greetingTitle
@@ -23,17 +25,39 @@ final class SelectInterestViewController: UIViewController {
         label.numberOfLines = 0
         return label
     }()
+    private var skView = SKView()
+    private var floatingCollectionScene = BubblesScene()
     private let finishButton = GotoSomewhereButton()
+
+    private var categoryList = [("art", "예술"), ("environment", "환경"), ("money", "돈"), ("animal", "동물"),
+                                ("company", "회사"), ("space", "우주"), ("economyIT", "IT"), ("economy", "경제"),
+                                ("robot", "로봇"), ("society", "사회"), ("broadcast", "방송"), ("culture", "문화"),
+                                ("history", "역사"), ("exercise", "운동"), ("health", "견강"), ("science", "과학"), ("world", "세계")]
 
     // MARK: - life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupFloatingSKView()
         self.setupLayout()
         self.setupButton()
     }
 
     // MARK: - func
+
+    private func setupFloatingSKView() {
+        self.skView = SKView(frame: UIScreen.main.bounds)
+        self.skView.backgroundColor = SKColor.white
+        self.view.addSubview(skView)
+
+        self.floatingCollectionScene = BubblesScene(size: skView.bounds.size)
+        self.floatingCollectionScene.topOffset = 0
+        self.skView.presentScene(self.floatingCollectionScene)
+
+        for indexNum in 0..<categoryList.count {
+            addBubble(category: categoryList[indexNum].0, tag: indexNum)
+        }
+    }
 
     private func setupButton() {
         self.finishButton.setupButtonContents(buttonImage: ImageLiteral.icoCheck,
@@ -59,4 +83,19 @@ final class SelectInterestViewController: UIViewController {
                                                            bottom: 46.adjustedHeight,
                                                            right: 40.adjustedWidth))
      }
+
+    @objc
+    func addBubble(category: String, tag: Int) {
+        let image = UIImage(named: category)!
+        let texture = SKTexture(image: image)
+        let newNode = BubbleNode.instantiate(texture: texture, tag: categoryList[tag].1)
+
+        newNode.selectedNode = {
+            newNode.texture = SKTexture(image: UIImage(named: "selected_\(category)")!)
+        }
+        newNode.deselectedNode = {
+            newNode.texture = SKTexture(image: UIImage(named: category)!)
+        }
+        self.floatingCollectionScene.addChild(newNode)
+    }
  }
