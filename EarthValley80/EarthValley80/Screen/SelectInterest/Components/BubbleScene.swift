576b4f0bb52e75ec570547e36a6be41756e89313
JI SOO <dlwltn815@gmail.com>
2022-12-12 00:38:13 +0900

    [CHORE] CGFloat extension 파일로 분리(#196)

--- EarthValley80/EarthValley80/Screen/SelectInterest/Components/BubbleScene.swift
+++ EarthValley80/EarthValley80/Screen/SelectInterest/Components/BubbleScene.swift
@@ -7,17 +7,6 @@
 
 import SpriteKit
 
-extension CGFloat {
-
-    public static func random() -> CGFloat {
-        return (CGFloat(arc4random()) / CGFloat(UInt32.max))
-    }
-
-    public static func random(min: CGFloat, max: CGFloat) -> CGFloat {
-        return CGFloat.random() * (max - min) + min
-    }
-}
-
 final class BubblesScene: SIFloatingCollectionScene {
     var topOffset: CGFloat = 0
 
