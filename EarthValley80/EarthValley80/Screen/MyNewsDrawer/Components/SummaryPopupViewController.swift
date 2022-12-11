e3f74c120d1f21fd40309d4308f56d144d4be1a7
JI SOO <dlwltn815@gmail.com>
2022-12-12 03:40:00 +0900

    [ADD] 뷰컨트롤러에 popup uiview 프로퍼티 추가 및 레이아웃 설정(#197)

--- EarthValley80/EarthValley80/Screen/MyNewsDrawer/Components/SummaryPopupViewController.swift
+++ EarthValley80/EarthValley80/Screen/MyNewsDrawer/Components/SummaryPopupViewController.swift
@@ -11,16 +11,25 @@ final class SummaryPopupViewController: UIViewController {
 
     // MARK: - property
 
+    private let popupView = SummaryPopupView()
 
     // MARK: - life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupLayout()
+        self.configureUI()
     }
 
     // MARK: - func
 
+    private func configureUI() {
+        self.view.backgroundColor = .black.withAlphaComponent(0.4)
+        self.popupView.setupLabel(newsTitle: "gkdl", mySummary: "gkdl")
+    }
 
+    private func setupLayout() {
+        self.view.addSubview(self.popupView)
+        self.popupView.constraint(centerX: self.view.centerXAnchor, centerY: self.view.centerYAnchor, padding: UIEdgeInsets.zero)
     }
 }
